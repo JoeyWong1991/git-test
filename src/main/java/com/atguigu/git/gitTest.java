75c1a0c077e77971412ad5a4b8917c68ab09e083
JoeyWong1991 <53230052+JoeyWong1991@users.noreply.github.com>
2021-09-05 22:52:51 +0800

    提交分支:master commit6

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -7,5 +7,7 @@ public class gitTest {
         System.out.println("我爱你2");
         System.out.println("我爱你3");
         System.out.println("hot-fix 我爱你");
+
+        System.out.println("master 冲突");
     }
 }
