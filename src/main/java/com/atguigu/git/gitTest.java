323868e6bb5b7d68807f69362fb7d4ef94c0e426
JoeyWong1991 <53230052+JoeyWong1991@users.noreply.github.com>
2021-09-05 22:42:49 +0800

    提交分支:hot-fix commit1

--- src/main/java/com/atguigu/git/gitTest.java
+++ src/main/java/com/atguigu/git/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("我爱你");
         System.out.println("我爱你2");
         System.out.println("我爱你3");
+        System.out.println("hot-fix 我爱你");
     }
 }
